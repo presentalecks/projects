d146220d0a38aa2ab600d101c6b2e0154df10d24
Alex Zamora <alex@adidaz.com>
2019-04-03 18:44:26 -0700

    added a test program

---
+++ struct.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+struct DIV 
+{
+	string name;
+	float sales;
+} test[4];
+
+int main()
+{
+	for(int i; i < 4; i++)
+	{
+		cout << "Please enter the sales for each \n";
+		cin >> test[i].sales;
+	}
+
+	for(int i; i < 4; i++)
+	{
+		cout << test[i].sales << " ";
+	}
+
+	return 0;
+}
